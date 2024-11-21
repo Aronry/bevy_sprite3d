b425fe57395ae7fd18b52f261a9867a0ecf7248d
keef <aronruutli@gmail.com>
2024-11-21 06:05:35 +0200

    no more roughness

--- src/lib.rs
+++ src/lib.rs
@@ -180,7 +180,7 @@ fn material(image: Handle<Image>, unlit: bool, emissive: LinearRgba) -> Standard
         emissive: emissive,
         base_color: emissive.into(),
         base_color_texture: Some(image),
-        perceptual_roughness: 0.,
+    //    perceptual_roughness: 0.,
         alpha_mode: AlphaMode::Mask(0.1),
         unlit: true,
         ..Default::default()
